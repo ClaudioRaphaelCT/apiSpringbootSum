fc4f5f514afa011b2662bd5d98f7068798d011d4
Claudio Raphael <claudioraphaelct@gmail.com>
2023-12-20 19:47:25 -0300

    Divisao de componentes

--- src/main/java/br/com/rapha/apigateway/MathController.java
+++ src/main/java/br/com/rapha/apigateway/MathController.java
@@ -1,5 +1,7 @@
 package br.com.rapha.apigateway;
 import br.com.rapha.apigateway.exceptions.UnsupportedMathOperationException;
+import br.com.rapha.apigateway.converters.NumberConverter;
+import br.com.rapha.apigateway.math.SimpleMath;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,28 +9,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MathController {
+   private final SimpleMath simpleMath = new SimpleMath();
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo
 
     ) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
-        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+        return simpleMath.sum(NumberConverter.convertToDouble(numberOne), NumberConverter.convertToDouble(numberTwo));
     }
 
-    private boolean isNumeric(String strNumber) {
-        if (strNumber == null) return false;
-        String number = strNumber.replaceAll(",", ".");
-        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
-    }
-
-    private Double convertToDouble(String strNumber) {
-        if (strNumber == null) return 0D;
-        String number = strNumber.replaceAll(",", ".");
-        if (isNumeric(number)) return Double.parseDouble(number);
-        return 0D;
+    @RequestMapping(value = "/decrease/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double decrease(
+            @PathVariable(value= "numberOne") String numberOne,
+            @PathVariable(value= "numberTwo") String numberTwo
+    ) throws Exception {
+        if (!NumberConverter.isNumeric(numberOne) || !NumberConverter.isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value");
+        }
+        return simpleMath.decrease(NumberConverter.convertToDouble(numberOne) , NumberConverter.convertToDouble(numberTwo));
     }
 }
---
+++ src/main/java/br/com/rapha/apigateway/math/SimpleMath.java
@@ -0,0 +1,10 @@
+package br.com.rapha.apigateway.math;
+
+public class SimpleMath {
+    public Double sum(Double numberOne, Double numberTwo){
+        return numberOne + numberTwo;
+    }
+    public Double decrease(Double numberOne, Double numberTwo){
+        return numberOne - numberTwo;
+    }
+}
